116aa07f22caf40bb2f7c90d44c68a88bd36c209
Tom Moseley <tommoseley@outlook.com>
2023-06-22 18:39:06 -0400

    Added main and blueprint

--- SimpleFactory/Program.cs
+++ SimpleFactory/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using SimpleFactory;
+using SimpleFactory.Components;
+using SimpleFactory.Blueprints;
+public static class Runner
+{
+    public static void Main(String[] Args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+        string? val = string.Empty;
+        Dictionary<string, int> Inventory = new Dictionary<string, int>();
+        List<Blueprint> Blueprints = new List<Blueprint>();
+        do
+        {
+            val = Console.ReadLine();
+            Console.WriteLine(val);
+
+        } while (val != "x");
+    }
+}
