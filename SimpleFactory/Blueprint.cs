6fa5a0026e273b1e059062f84b401c30748ba3e3
Tom Moseley <tommoseley@outlook.com>
2023-06-22 21:22:42 -0400

    Building Blueprint

---
+++ SimpleFactory/Blueprint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    internal class Blueprint
+    {
+        public Blueprint (string Name)
+        {
+
+        }
+        public string Name { get; set; }
+        public Dictionary<string, int> Recipe = new Dictionary<string, int> ();
+        public bool CanMake(Dictionary<string, int> inventory)
+        {
+            foreach (string key in Recipe.Keys)
+            {
+                if (inventory.ContainsKey (key) == false || inventory[key] < Recipe[key])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+    }
+}
